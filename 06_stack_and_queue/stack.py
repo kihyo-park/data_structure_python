c0c2d5660bfa89f3baebed20c771b7f29c0fa6c9
Kihyo Park <khpark9305@gmail.com>
2025-07-07 00:31:05 +0900

    modify stack (in progress)

--- 06_stack_and_queue/stack.py
+++ 06_stack_and_queue/stack.py
@@ -1,16 +1,26 @@
 class Stack:
     def __init__(self, array_size : int, top : int, array : list):
-        self.array_size = array_size
-        self.top = top
-        self.array = array
+        self.array = list(array)
+        self.array_size = len(array)
+        self.top = array[-1] # 이거 안 써도 되는 것 같은데 지우기...
     def __str__(self):
         return f"Array: {self.array}, Top element: {self.top}, Array size: {self.array_size}"
 
-def StackPush(Stack, value):
-    if Stack.top == Stack.array_size - 1:
-        Stack.array_size += 1    
-    Stack.top += 1
-    Stack.array[Stack.top] = value
+    def push(self, value):
+        if self.top == self.array_size - 1:
+            new_size = self.array_size * 2
+            new_array = [None] * new_size 
+        
+            for i in range(self.array_size):
+                new_array[i] = self.array[i]
+        
+            self.array = new_array
+            self.array_size = new_size
 
-s = Stack(4, 3, [0, 1, 2, 3])
+        self.top += 1
+        self.array[self.top] = value
+
+s = Stack(4, 5, [0])
 print(s)
+#s.push(6)
+#print(s)
