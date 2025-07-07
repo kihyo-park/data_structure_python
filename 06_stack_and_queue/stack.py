6bf2daca4d0536f18aa8a9bf1e0a4fa572be1c7a
Kihyo Park <khpark9305@gmail.com>
2025-07-07 15:42:07 +0900

    added Push properly, IsEmpty and IsFull

--- 06_stack_and_queue/stack.py
+++ 06_stack_and_queue/stack.py
@@ -1,26 +1,43 @@
 class Stack:
-    def __init__(self, array_size : int, top : int, array : list):
+    def __init__(self, array_size : int, array : list):
         self.array = list(array)
         self.array_size = len(array)
-        self.top = array[-1] # 이거 안 써도 되는 것 같은데 지우기...
+        self.max_size = array_size
+        self.top = self.array_size - 1 # 'top' is the INDEX of the top element
     def __str__(self):
-        return f"Array: {self.array}, Top element: {self.top}, Array size: {self.array_size}"
+        return f"Array: {self.array}, Top index: {self.top}, Array size: {self.array_size}"
 
-    def push(self, value):
-        if self.top == self.array_size - 1:
-            new_size = self.array_size * 2
-            new_array = [None] * new_size 
+    def IsEmpty(self):
+        if self.array_size == 0:
+            return True
+
+    def IsFull(self):
+        if self.array_size == self.max_size:
+            return True
+
+    def Push(self, value):
+        if self.IsFull() == True:
+            print("Stack is full. Adding another empty slot...")
+            old_size = self.max_size
         
-            for i in range(self.array_size):
+            self.max_size = old_size + 1
+        
+            new_array = [None] * self.max_size
+
+            self.array_size = len(new_array)
+            
+            for i in range(old_size):
                 new_array[i] = self.array[i]
         
-            self.array = new_array
-            self.array_size = new_size
+        self.array = new_array
+        print(f"Added. New Max Size: {self.max_size}")
 
         self.top += 1
         self.array[self.top] = value
 
-s = Stack(4, 5, [0])
+s = Stack(4, [0, 1, 2, 3])
+print(s)
+s.Push(6)
 print(s)
 #s.push(6)
 #print(s)
